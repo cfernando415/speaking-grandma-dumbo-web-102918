303c21309f46a2c7392d1e3dd6aaa7d9e051e708
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-05 21:14:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@ def speak_to_grandma(phrase)
   if(!(phrase == phrase.upcase))
     "HUH? SPEAK UP, SONNY!"
   elsif(phrase == "I LOVE YOU GRANDMA!")
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   else
     puts "NO, NOT SINCE 1938!"
   end
